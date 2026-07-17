0ba32d7e1cf6e88337084737698804a4b11f3ff4
wayne120 <mliliany001@gmail.com>
2026-07-17 02:14:11 -0500

    Add configuration file for WayneMail app

---
+++ Config.js
@@ -0,0 +1,22 @@
+const config = {
+  appName: "WayneMail",
+  domain: "waynemail.com",
+  currency: "NGN",
+
+  plans: {
+    normal: {
+      price: 200,
+      duration: 7
+    },
+    premium: {
+      price: 400,
+      duration: 7
+    },
+    premiumPlus: {
+      price: 500,
+      duration: 7
+    }
+  }
+};
+
+module.exports = config;
