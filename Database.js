9c40fa99579747f0c6a522df62bad217346da315
wayne120 <mliliany001@gmail.com>
2026-07-17 02:10:32 -0500

    Initialize Database.js with user and plan data

---
+++ Database.js
@@ -0,0 +1,22 @@
+const database = {
+  users: [],
+  plans: [
+    {
+      name: "Normal",
+      price: 200,
+      duration: 7
+    },
+    {
+      name: "Premium",
+      price: 400,
+      duration: 7
+    },
+    {
+      name: "Premium+",
+      price: 500,
+      duration: 7
+    }
+  ]
+};
+
+module.exports = database;
