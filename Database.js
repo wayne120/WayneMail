75f23b1e1fa8e763c3546068e090cf8c51e40d44
wayne120 <mliliany001@gmail.com>
2026-07-17 02:49:39 -0500

    Refactor plans structure in Database.js

--- Database.js
+++ Database.js
@@ -1,22 +1,33 @@
 const database = {
   users: [],
-  plans: [
-    {
+
+  plans: {
+    normal: {
       name: "Normal",
       price: 200,
       duration: 7
     },
-    {
+
+    premium: {
       name: "Premium",
       price: 400,
       duration: 7
     },
-    {
+
+    premiumPlus: {
       name: "Premium+",
       price: 500,
       duration: 7
     }
-  ]
+  },
+
+  addUser(user) {
+    this.users.push(user);
+  },
+
+  getUsers() {
+    return this.users;
+  }
 };
 
 module.exports = database;
